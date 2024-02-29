319e7bd157276d2027900032f27f1d9fdc9b6387
Sunwish <isunwish@foxmail.com>
2024-02-29 16:49:33 +0800

    修复腾讯源的中文乱码的问题

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module TraderHelperCore
 
 go 1.21.6
+
+require golang.org/x/text v0.14.0 // indirect
--- staging/dataSource/tencentDataSource.go
+++ staging/dataSource/tencentDataSource.go
@@ -3,8 +3,12 @@ package dataSource
 import (
 	formatter "TraderHelperCore/staging/formatter"
 	urlBuilder "TraderHelperCore/staging/urlBuilder"
+	"bytes"
 	"io"
 	"net/http"
+
+	"golang.org/x/text/encoding/simplifiedchinese"
+	"golang.org/x/text/transform"
 )
 
 type TencentDataSource struct{}
@@ -23,16 +27,27 @@ func TencentDataFetcher(url string) string {
 	if err != nil {
 		return ""
 	}
-	req.Header.Set("Accept-Encoding", "GB2312")
 	req.Header.Set("Referer", "https://gu.qq.com/")
 	resp, err := client.Do(req)
 	if err != nil {
 		return ""
 	}
 	defer resp.Body.Close()
+
 	bodyBytes, err := io.ReadAll(resp.Body)
+	bodyBytes, _ = gbkToUtf8(bodyBytes)
 	if err != nil {
 		return ""
 	}
 	return string(bodyBytes)
+}
+
+// GBK 转 UTF-8
+func gbkToUtf8(s []byte) ([]byte, error) {
+	reader := transform.NewReader(bytes.NewReader(s), simplifiedchinese.GBK.NewDecoder())
+	d, e := io.ReadAll(reader)
+	if e != nil {
+		return nil, e
+	}
+	return d, nil
 }
