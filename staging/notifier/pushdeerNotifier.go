3cced2d8d78dc6e1706debb01938c1279d0a9c73
Sunwish <isunwish@foxmail.com>
2024-02-29 18:19:48 +0800

    新增Pushdeer Notifier实现

---
+++ staging/notifier/pushdeerNotifier.go
@@ -0,0 +1,30 @@
+package notifier
+
+import (
+	"TraderHelperCore/api"
+	"fmt"
+	"net/http"
+	"net/url"
+)
+
+type pushdeerNotifier struct {
+	baseUrl string
+	appKey  string
+}
+
+func NewPushdeerNotifier(baseUrl string, appKey string) api.Notifier {
+	return &pushdeerNotifier{
+		baseUrl: baseUrl,
+		appKey:  appKey,
+	}
+}
+
+func (p pushdeerNotifier) Notify(title string, message string) {
+	fullUrl := p.baseUrl + "push?pushkey=" + p.appKey + "&text=" + url.QueryEscape(title) + "&desp=" + url.QueryEscape(message) + "&type=markdown"
+	response, err := http.Get(fullUrl)
+	if err != nil {
+		fmt.Println("消息推送失败：", err)
+		return
+	}
+	defer response.Body.Close()
+}
